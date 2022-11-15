0659fd8f7eba6c80aa58615df4bdd40e340803e8
Benjamin Doberset <benjamin.doberset.pro@gmail.com>
2022-11-15 14:52:36 +0100

    refectoring

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tasks
+
 end
+
+
+# get "tasks", to: "tasks#index"
+# get "tasks/:id", to: "tasks#show", as: :task
+# get "tasks/new",  to: "tasks#new"
+# post "tasks", to: "tasks#create"
+# get "task/:id/edit", to: "tasks#edit", as: :task_edit
+# patch "tasks/:id", to: "tasks#update"
+# delete "tasks/:id", to: "tasks#destroy"
